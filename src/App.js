b05d8d1bb6862300abfe64fd27e74e8a23c45023
ShimJiHoon <wlgns2223@gmail.com>
2024-06-25 22:59:24 +0900

    chore: initial commits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Items } from "./item";
+
+// https://localhost
 
 function App() {
   return (
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello
         </a>
       </header>
     </div>
